1d0047d8f254af3ebb52159f88cc19c111c540af
CuongDo <cuongdoleviet@gmail.com>
2019-11-14 22:45:48 +0700

    improve cat eat method

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 var cat = require('./cat')
+var horse = require('./horse')
 
 var mickey = new cat('Mickey')
 var tom = new cat('Tommy')
+var ngua = new horse('kiki')
 
 tom.eat('jerry')
 mickey.eat(tom)
 
+try{
+	tom.eat(ngua);
+}catch(error){
+	console.log('error :' + error);
+}
 
 console.log(tom)
